3481589dd5068d6119c33ffd5a7b8a3797e9a3e2
Louis Baptiste <louisvis@ymail.com>
2020-06-01 19:53:09 -0400

    auto scrool v1, new speech bubbles

--- offcanvas.js
+++ offcanvas.js
@@ -32,19 +32,39 @@ function displayGame (){
 
      </div>
    </div>`
+   let customBubble_tree = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
+     <img src="https://louis7.github.io/bubble.2/double_bubble2.jpg" class="card-img" alt="...">
+
+     </div>
+   </div>`
+   let customBubble_four = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
+     <img src="https://louis7.github.io/bubble.2/double_bubble3.jpg" class="card-img" alt="...">
+
+     </div>
+   </div>`
    setTimeout(function(){
    document.getElementById('main').innerHTML += user_bubble;
+   window.scrollTo(0,document.body.scrollHeight);
+
  },500); //delay is in milliseconds
 
  setTimeout(function(){
    document.getElementById('main').innerHTML += ai_answer;
+   window.scrollTo(0,document.body.scrollHeight);
+
  },2000); //delay is in milliseconds
 
-   setTimeout(function(){
-  document.getElementById('main').innerHTML += customBubble_two;
-},3500); //delay is in milliseconds
 
+setTimeout(function(){
+document.getElementById('main').innerHTML += customBubble_tree;
+window.scrollTo(0,document.body.scrollHeight);
 
+},3500);
+setTimeout(function(){
+document.getElementById('main').innerHTML += customBubble_four;
+window.scrollTo(0,document.body.scrollHeight);
+
+},3500);
 }
 
 function saveMessage(){
@@ -65,3 +85,7 @@ const form = `<div id = "nameform" class="mx-auto pt-5" style="width: 200px;">
       </form>
       </div>
  </div>`
+function scroll(){
+  window.scrollTo(0,document.body.scrollHeight);
+
+}
