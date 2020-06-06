4ba32cf76fc18f976eaf226aec3dd0a95aafaaeb
Louis Baptiste <louisvis@ymail.com>
2020-06-06 03:43:52 -0400

    fix sized issues, loading animation

--- offcanvas.js
+++ offcanvas.js
@@ -8,7 +8,64 @@ function displayForm (){
     document.getElementById('main').innerHTML = form;
 }
 
+//This is a test to display the greet Function outside of the display game function
+
+let loading = `<div id ="loading-dots" class="loading-dots ">
+  <div class="loading-dot delay-0"></div>
+  <div class="loading-dot delay-1"></div>
+  <div class="loading-dot delay-2"></div>
+</div>`
+
+let ai_answer0 = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
+  <img src="https://louis7.github.io/bubble.2/test_bubble.jpg" class="card-img" alt="...">
+  <div class="card-img-overlay  b w-50 h-50 mx-auto my-auto  d-inline-flex p-0 justify-content-center" >
+
+    <p class="align-self-center mt-3">hello?</p>
+
+  </div>
+</div>`
+let ai_answer1 = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
+  <img src="https://louis7.github.io/bubble.2/test_bubble.jpg" class="card-img" alt="...">
+  <div class="card-img-overlay  b w-50 h-50 mx-auto my-auto  d-inline-flex p-0 justify-content-center" >
+
+    <p class="align-self-center mt-3">your name?</p>
+
+  </div>
+</div>`
+function loading_test(){
+  setTimeout(function(){
+    document.getElementById('main').innerHTML += loading;
+    window.scrollTo(0,document.body.scrollHeight);
+
+  },1000);
+  setTimeout(function(){
+    document.getElementById('loading-dots').remove();
+
+  },2005);
+}
+
+function displayfirst(){
+  loading_test()
+  setTimeout(function(){
+    document.getElementById('main').innerHTML += ai_answer0;
+    window.scrollTo(0,document.body.scrollHeight);
+
+  },3000);
+  setTimeout(function(){
+    document.getElementById('main').innerHTML += ai_answer1;
+    window.scrollTo(0,document.body.scrollHeight);
+
+  },4000);
+}
+displayfirst();
+
+//This is a test of display function outside of the display game function
+
+
+
+
 function displayGame (){
+
    let ai_answer = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
      <img src="https://louis7.github.io/bubble.2/test_bubble.jpg" class="card-img" alt="...">
      <div class="card-img-overlay  b w-50 h-50 mx-auto my-auto  d-inline-flex p-0 justify-content-center" >
@@ -27,27 +84,19 @@ function displayGame (){
        </div>
      </div>
     </div>`
-   let customBubble_two = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
-     <img src="https://louis7.github.io/bubble.2/double_bubble.jpg" class="card-img" alt="...">
-
-     </div>
-   </div>`
-   let customBubble_tree = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
-     <img src="https://louis7.github.io/bubble.2/double_bubble2.jpg" class="card-img" alt="...">
-
-     </div>
-   </div>`
-   let customBubble_four = `<div class="card bg-dark  w-25  border m-2 border-white bg-white">
+   let customBubble_two = `<div class="card bg-dark  w-50 border m-2 border-white bg-white">
      <img src="https://louis7.github.io/bubble.2/double_bubble3.jpg" class="card-img" alt="...">
 
-     </div>
    </div>`
+
+
+
    setTimeout(function(){
+     loading_test()
    document.getElementById('main').innerHTML += user_bubble;
    window.scrollTo(0,document.body.scrollHeight);
 
  },500); //delay is in milliseconds
-
  setTimeout(function(){
    document.getElementById('main').innerHTML += ai_answer;
    window.scrollTo(0,document.body.scrollHeight);
@@ -56,15 +105,12 @@ function displayGame (){
 
 
 setTimeout(function(){
-document.getElementById('main').innerHTML += customBubble_tree;
+  loading_test()
+document.getElementById('main').innerHTML += customBubble_two;
 window.scrollTo(0,document.body.scrollHeight);
 
 },3500);
-setTimeout(function(){
-document.getElementById('main').innerHTML += customBubble_four;
-window.scrollTo(0,document.body.scrollHeight);
 
-},3500);
 }
 
 function saveMessage(){
